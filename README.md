2dab86d5b92c22cea02d6624564e3b0313f8c6f6
mohsafwat23 <safwat@wisc.edu>
2022-08-31 22:31:22 +0200

    some instructions

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # probabilistic-robots-
+
+#### Installs ####:
+#### Joint State publisher and Xacro
+`sudo apt install ros-foxy-xacro ros-foxy-joint-state-publisher-gui`
+
+#### Gazebo and CV bridge install
+`sudo apt install gazebo11 libgazebo11 libgazebo11-dev`
+`sudo apt install ros-foxy-gazebo-ros-pkgs ros-foxy-cv-bridge`
+
+
+#### Sourcing ####
+`source /opt/ros/foxy/setup.bash`
+
+`export GAZEBO_MODEL_PATH=${PWD}/install/gazebo_envs/share/gazebo_envs/models`
+
+`source /usr/share/gazebo/setup.sh`
+
+#### Commands ####
+#### Build
+`colcon build --symlink-install`
+
+#### Run the particle filter
+`ros2 launch gazebo_envs robot.launch.py`
+
+#### Control Robot with keyboard
+`ros2 run teleop_twist_keyboard teleop_twist_keyboard --ros-args -r /cmd_vel:=/robot/cmd_vel`
+
+#### Kill gazebo server
+`killall -9 gzserver`
--- diff_rob_model/src/pf.cpp
+++ diff_rob_model/src/pf.cpp
@@ -2,13 +2,24 @@
 
 PF::PF()
 {
+    //number of particles
     n_particles = 1500;
+
+    //resize all vectors to n_particles
     particles.resize(n_particles);
     weights.resize(n_particles);
     error.resize(n_particles);
+
+    //width of robot (todo:create global param)
     Width = 0.4;
+
+    //avoids NaN
     dt = 0.00;
+
+    //initialize weight sum
     weight_total = 0.0;
+
+    //avoids NaN
     u_enc = Eigen::Vector2d::Zero();
 }
 
@@ -17,17 +28,24 @@ void PF::genParticles(struct limits range)
     // Initializing of uniform_real_distribution class
     std::default_random_engine generator;
 
+    //particles state x,y,theta
     std::uniform_real_distribution<double> distributionX(range.Xlower, range.Xupper);
     std::uniform_real_distribution<double> distributionY(range.Ylower, range.Yupper);
     std::uniform_real_distribution<double> distributionTheta(-pi, pi);
+
     for(int i=0; i < n_particles; i++)
     {
+        //particle structure
         particle p;
         p.i = i;
+
+        //weights placeholder
         p.weight = 1.0;
         p.x = distributionX(generator);
         p.y = distributionY(generator);
         p.theta = distributionTheta(generator);
+
+        //vector of particles
         particles[i] = p;
 
     }
@@ -37,9 +55,13 @@ void PF::genParticles(struct limits range)
 void PF::prediction(double std_model[])
 {
     std::default_random_engine gen;
+
+    //for each particle, propogate
     for(int i=0; i<n_particles; i++)
     {
+        //vector address
         particle *p = &particles[i];
+
         //this also works
         // particles[i].x += cos(particles[i].theta)*dt*((u_enc(0)+u_enc(1))/2.0);
         // particles[i].y += sin(particles[i].theta)*dt*((u_enc(0)+u_enc(1))/2.0);
@@ -51,6 +73,7 @@ void PF::prediction(double std_model[])
         //this handles angle wrapping
         p->theta = atan2(sin(p->theta), cos(p->theta));
 
+        //gaussian distribution 
         std::normal_distribution<double> dist_x(p->x, std_model[0]);
         std::normal_distribution<double> dist_y(p->y, std_model[1]);
         std::normal_distribution<double> dist_theta(p->theta, std_model[2]);
@@ -69,12 +92,18 @@ void PF::weigh(std::vector<double> landmark, const double dist, const double bea
 {
     measurement(0) = dist;
     measurement(1) = bearing;
+
+    //temporary: avoids readding 
     weight_total = 0.0;
+
     for(int i=0; i<n_particles; i++)
     {
         particle *p = &particles[i];
+
+        //observation model
         measurement_hat(0) = sqrt(pow((p->x - landmark[0]),2) + pow((p->y - landmark[1]),2));
         measurement_hat(1) = atan2((landmark[1]-p->y), (landmark[0] - p->x)) - p->theta; 
+        
         //this handles angle wrapping
         measurement_hat(1) = atan2(sin(measurement_hat(1)), cos(measurement_hat(1)));
         p->weight *= gaussian(measurement(0), std_sensor[0], measurement_hat(0));
@@ -90,17 +119,23 @@ void PF::weigh(std::vector<double> landmark, const double dist, const double bea
     for(int i=0; i<n_particles; i++)
     {
         particle *p = &particles[i];
-        p->weight += 1.e-300;   //this stops from particle weights going to 0
+
+        //this stops from particle weights going to 0
+        p->weight += 1.e-300;   
+
+        //normalize
         p->weight /= weight_total;
         weights[i] = p->weight;
     }
 
 }
 
+//Todo: better resampling algorithm
 void PF::resample()
 {
     std::default_random_engine gen;
 
+    //resample with probabilty of particle is its weight
     std::discrete_distribution<int> distribution(weights.begin(), weights.end());
     std::vector<particle> resampled_particles;
     for(int i=0; i<n_particles; i++)
